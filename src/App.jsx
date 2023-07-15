6cc8276315098ea93224d2068bf4418de5ebc39e
jwd222 <sji.2.sami@gmail.com>
2023-07-16 04:35:19 +0600

    1st commit with a little functionatlity

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './styles.css'
+
+export default function App() {
+  const [newItem, setNewItem] = useState('')
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form" action="">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To do List</h1>
+      <ul className="list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
